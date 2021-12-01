024486fee2592517a28670812de0e41998557278
AlexRedSky <redxsky59@gmail.com>
2021-12-01 21:27:14 +0300

    Added Day 1 puzzles

---
+++ AOC1/Program.cs
@@ -0,0 +1,36 @@
+﻿using AOC1;
+
+
+
+Console.WriteLine("Hello, this is Advent Of Code 2021 1 day puzzle solution! Please enter puzzle input. Enter 'F' to finish.");
+List<int> measurements = new();
+
+while (true)
+{
+    var input = Console.ReadLine() ?? string.Empty;
+    if (int.TryParse(input, out var parsed))
+    {
+        measurements.Add(parsed);
+    }
+    else if (input.Equals("F", StringComparison.OrdinalIgnoreCase))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Looks like you entered something wrong. Please try again.");
+    }
+}
+
+try
+{
+    var increases = SonarSweep.CountIncreasesWindow(measurements, 3);
+    Console.WriteLine($"There are {increases} measurements that are larger than the previous measurement.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Sorry, something went wrong. {ex.Message}");
+}
+
+
+
---
+++ AOC1/SonarSweep.cs
@@ -0,0 +1,51 @@
+﻿namespace AOC1
+{
+    public static class SonarSweep
+    {
+        //Puzzle 1 solution
+        public static int CountIncreasesSimple(List<int> measurements)
+        {
+            if (measurements.Count < 2)
+            {
+                throw new ArgumentException("There is not enough data.");
+            }
+
+            var increases = 0;
+            for (int i = 1; i < measurements.Count; i++)
+            {
+                if (measurements[i] > measurements[i - 1])
+                {
+                    increases++;
+                }
+            }
+
+            return increases;
+        }
+
+        //Puzzle 2 solution
+        public static int CountIncreasesWindow(List<int> measurements, int windowSize)
+        {
+            if (measurements.Count < windowSize + 1)
+            {
+                throw new ArgumentException("There is not enough data.");
+            }
+
+            var increases = 0;
+            var prevSum = measurements.Take(windowSize).Sum();
+
+            for (int i = 1; i < measurements.Count - (windowSize - 1); i++)
+            {
+                var currentSum = measurements.Skip(i).Take(windowSize).Sum();
+
+                if (currentSum > prevSum)
+                {
+                    increases++;
+                }
+
+                prevSum = currentSum;
+            }
+
+            return increases;
+        }
+    }
+}
